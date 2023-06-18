fa1753bed4f134a9f2d57d999994575bb8ed1081
HamidSadEsf <133522207+HamidSadEsf@users.noreply.github.com>
2023-06-18 20:45:57 +0200

    adding Clustering and NearestNeigbor of one movie

--- ContentBasedRec.py
+++ ContentBasedRec.py
@@ -48,7 +48,7 @@ def condition(x):
         return x[-6:-2]
     else:
         return np.nan
-df_movies['releaseyear'] = df_movies['title'].apply(condition)
+df_movies['releaseyear'] = df_movies['title'].apply(condition).fillna(1993)
 
 scaler02 = MinMaxScaler()
 df_movies.releaseyear = scaler02.fit_transform(df_movies[['releaseyear']])
@@ -56,3 +56,38 @@ df_movies.releaseyear = scaler02.fit_transform(df_movies[['releaseyear']])
 df_ContBaseRec = pd.merge(df_gtagscore, df_movies, how='inner', on='movieId').set_index('movieId')
 
 df_ContBaseRec.info()
+
+#Clsutering
+#importing the library
+
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+
+## training the Model
+
+kmeans = KMeans(n_clusters=18, n_init='auto')
+kmeans.fit(df_ContBaseRec)
+labels = kmeans.labels_ # getting the movis labled with the corresponding cluster.
+#making a new df of the movies with the labels and MovieId, title and release year.
+df_labeledMovies= pd.DataFrame({'movieId': df_ContBaseRec.index, 'labels': labels})\
+                    .merge(df_movies, on='movieId').set_index('movieId')
+
+# recommending the nearest neighbors within the given cluster with the example "Network (1975)
+
+from sklearn.neighbors import NearestNeighbors
+
+#specifying the movie
+given_movie_id =df_labeledMovies[df_labeledMovies.title.str.contains(r'\bNetwork\b.{0,9}$', case=False)].labels.item()
+given_movie_cluster = df_labeledMovies.loc[given_movie_id, "labels"] # getting the cluster
+movies_in_cluster_indices = df_labeledMovies[df_labeledMovies["labels"] == given_movie_cluster].index.tolist()
+movies_in_cluster_relevance = df_ContBaseRec.loc[movies_in_cluster_indices] # Filter the relevance data based on movies in the same cluster
+
+# Train the Nearest Neighbors algorithm
+nn = NearestNeighbors(n_neighbors=6)  # 5 neighbors + the given movie itself
+nn.fit(movies_in_cluster_relevance)
+distances, indices = nn.kneighbors([df_ContBaseRec.loc[given_movie_id]]) # finding the nearest neigbors
+nearest_neighbor_ids = [movies_in_cluster_indices[i] for i in indices[0]] # Get the IDs of the nearest neighbors
+nearest_neighbor_ids.remove(given_movie_id) # Remove the given movie from the list of neighbors
+
+df_labeledMovies.loc[nearest_neighbor_ids]
