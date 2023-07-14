d50ce1679db83991e1f9879140d273fc95bef48c
HamidSadEsf <133522207+HamidSadEsf@users.noreply.github.com>
2023-07-14 21:17:17 +0200

    establishing the function

--- model/ColdStarter.py
+++ model/ColdStarter.py
@@ -1,2 +1,7 @@
-def get_ranking(df):
-    return new_def
+import pandas as pd
+def cold_starters(df, amount = 10):
+    def Min_Max(obj):
+        nor_obj =  (obj - obj.min()) / (obj.max() - obj.min())
+        return nor_obj
+    df['score'] = Min_Max(df.avg_rating) + Min_Max(df.rating_amount) + Min_Max(df.realease_year)
+    return df
