90a1f286548b33d4ee37563999fc20bf26835e87
HamidSadEsf <133522207+HamidSadEsf@users.noreply.github.com>
2023-09-29 21:20:29 +0200

    final correction on common_preprocessing

--- data_script/common_preprocessing.py
+++ data_script/common_preprocessing.py
@@ -7,8 +7,3 @@ df = get_df_ContBaseRec()
 
 from clustering_content_based import get_labeledMovies
 get_labeledMovies(df, clusters=15)
-
-import pandas as pd
-from content_based_matrix import get_CBMatrix
-get_CBMatrix()
-    
